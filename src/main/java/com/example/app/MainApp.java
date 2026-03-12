f3c38e0fca91eca1f529373d196be7a82da58622
SrividyaTikka <67415800+SrividyaTikka@users.noreply.github.com>
2026-03-12 10:47:42 +0530

    Create MainApp class for JavaFX application

---
+++ src/main/java/com/example/app/MainApp.java
@@ -0,0 +1,22 @@
+package com.example.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/app/hello.fxml"));
+        Scene scene = new Scene(loader.load());
+        primaryStage.setTitle("Simple Java Frontend");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
